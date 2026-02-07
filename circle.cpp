7ddf80018c18d0955543b13ad1f38ab98fc1ab5d
Abdullah <abdullah.irshad.79462@gmail.com>
2026-02-07 22:15:43 +0500

    Automated Everything

--- circle.cpp
+++ circle.cpp
@@ -1,74 +1,87 @@
+#include <windows.h>
+#include <shellapi.h>
+#include <thread>
 #include <iostream>
 #include <cmath>
-#include <windows.h>
 using namespace std;
 
 void holdLeftMouseButton() {
     INPUT input = {0};
     input.type = INPUT_MOUSE;
     input.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
-
     SendInput(1, &input, sizeof(INPUT));
 }
 void releaseLeftMouseButton() {
     INPUT input = {0};
     input.type = INPUT_MOUSE;
     input.mi.dwFlags = MOUSEEVENTF_LEFTUP;
-
     SendInput(1, &input, sizeof(INPUT));
 }
 
-const float PI = 3.14159265;
+const float PI = 3.14159265f;
 void drawCircle(float radius, int centerX, int centerY, int steps = 300) {
     holdLeftMouseButton();
     for (int i = 0; i <= steps; i++) {
         float theta = (2 * PI * i) / steps;
         int x = static_cast<int>(centerX + radius * cos(theta));
         int y = static_cast<int>(centerY + radius * sin(theta));
-        Sleep(1);
         SetCursorPos(x, y);
+        Sleep(1);
     }
     releaseLeftMouseButton();
 }
 
-
-BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lParam) {
-    char title[256];
-    GetWindowTextA(hwnd, title, sizeof(title));
-    
-    if (strstr(title, "Draw")) { //change the window title
-        ShowWindow(hwnd, SW_RESTORE);
-        SetForegroundWindow(hwnd);
-        ShowWindow(hwnd, SW_SHOWMAXIMIZED);
-        return FALSE;
-    }
-
-    return TRUE;
-}
-
-POINT getClickPosition() {
-    POINT pt = {0};
-    while (true) {
-        if (GetAsyncKeyState(VK_LBUTTON) & 0x8000) {
-            GetCursorPos(&pt);
-            break;
+HWND FindBrowserWindow(DWORD pid) {
+    HWND hwndFound = NULL;
+    EnumWindows([](HWND hwnd, LPARAM lParam) -> BOOL {
+        DWORD windowPID;
+        GetWindowThreadProcessId(hwnd, &windowPID);
+        HWND* pHwnd = reinterpret_cast<HWND*>(lParam);
+        if (windowPID == *(DWORD*)pHwnd && IsWindowVisible(hwnd)) {
+            *pHwnd = hwnd;
+            return FALSE;
         }
-        Sleep(10);
-    }
-    Sleep(200);
-    return pt;
+        return TRUE;
+    }, (LPARAM)&hwndFound);
+    return hwndFound;
 }
+
 int main() {
+    string url = "https://neal.fun/perfect-circle/";
 
-    EnumWindows(EnumWindowsProc, 0);
-    Sleep(1000);
+    SHELLEXECUTEINFOA shExecInfo = {0};
+    shExecInfo.cbSize = sizeof(SHELLEXECUTEINFOA);
+    shExecInfo.fMask = SEE_MASK_NOCLOSEPROCESS;
+    shExecInfo.hwnd = NULL;
+    shExecInfo.lpVerb = "open";
+    shExecInfo.lpFile = "chrome.exe";
+    shExecInfo.lpParameters = url.c_str();
+    shExecInfo.nShow = SW_SHOWNORMAL;
 
-    POINT center = getClickPosition();
+    if (!ShellExecuteExA(&shExecInfo)) {
+        cerr << "Failed to open browser. Make sure chrome.exe is in PATH.\n";
+        return 1;
+    }
 
-    float radius = 150;
+    DWORD pid = GetProcessId(shExecInfo.hProcess);
+    keybd_event(VK_F11, 0, 0, 0);
+    Sleep(100);
+    keybd_event(VK_F11, 0, KEYEVENTF_KEYUP, 0);
 
-    SetCursorPos(center.x + static_cast<int>(radius), center.y);
-    Sleep(1000);
+    int screenX = GetSystemMetrics(SM_CXSCREEN);
+    int screenY = GetSystemMetrics(SM_CYSCREEN);
+    int centerX = screenX / 2;
+    int centerY = screenY / 2;
+    SetCursorPos(centerX, centerY);
+    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0); 
+    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0); 
+    Sleep(500);
+    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0); 
+    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+    
 
-    drawCircle(radius, center.x, center.y);
+    int radius = 290;
+    SetCursorPos(centerX + static_cast<int>(radius), centerY);
+    drawCircle(radius, centerX, centerY, 300);
+    return 0;
 }
